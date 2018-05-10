8b3ab5bfb2af24bdf706eb4222a52578c2b2edf4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-10 13:15:45 -0700

    Knock Knock, Greeter, Everything Is Awesome, I know what you did last summer

---
+++ src/KnockKnock.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Knock Knock");
+	String answer = JOptionPane.showInputDialog("Who's There?");
+			String who = JOptionPane.showInputDialog(answer +" who?");   
+			
+			
+			
+			
+			
+			
+}
+}
